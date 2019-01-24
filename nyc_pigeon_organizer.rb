a5b79b3a4f706334241460b987bdbbf31a121239
eselbeus <eselbeus@gmail.com>
2019-01-24 01:14:13 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,13 @@
+require "pry"
+
 def nyc_pigeon_organizer(data)
   # write your code here!
+  bird_hash = {}
+  data[:gender].each do |gend, birds|
+    birds.each do |bird|
+      bird_hash[bird] = {}
+    end
+  end
+  
+  bird_hash
 end
